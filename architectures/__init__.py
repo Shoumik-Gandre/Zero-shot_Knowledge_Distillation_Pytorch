c7e26cc35df3fbdd34de605fe28e88cc514bf77f
Shoumik-Gandre <shoumikgandre@gmail.com>
2023-06-26 15:30:11 -0700

    add examples/tabular and a file that executes it

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -1,6 +1,7 @@
 import torch
 from architectures.resmlp import ResMLP
 from architectures.lenet import LeNet5
+from architectures.resnet import ResNet18
 
 def get_model(name: str) -> torch.nn.Module:
     match name:
@@ -8,4 +9,6 @@ def get_model(name: str) -> torch.nn.Module:
             return LeNet5()
         case 'resmlp':
             return ResMLP(32*32, 10)
+        case 'resnet':
+            return ResNet18(10, 1) 
     raise ValueError()
